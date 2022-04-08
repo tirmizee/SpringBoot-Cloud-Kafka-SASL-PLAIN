821ad791450c7a8a80b5d68e182ac08370b70c15
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2022-04-09 04:57:04 +0700

    update project

--- src/main/java/com/tirmizee/SpringBootCloudKafkaSaslPlainApplication.java
+++ src/main/java/com/tirmizee/SpringBootCloudKafkaSaslPlainApplication.java
@@ -1,4 +1,4 @@
-package com.tirmizee.SpringBootCloudKafkaSASL;
+package com.tirmizee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
